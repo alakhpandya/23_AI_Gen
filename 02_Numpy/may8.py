e1c905f16b28227bf0e04edd3ac483b352300fc4
alakhpandya <alakhpandya@gmail.com>
2023-05-08 22:45:02 +0530

    may8

---
+++ 02_Numpy/may8.py
@@ -0,0 +1,59 @@
+import numpy as np
+"""
+a = np.array([1, 2, 3, 4, 5])
+print(a)
+b = np.array([[1, 2], [3, 4]])
+print(b)
+c = np.array([[1, 2], [3, 4], [5, 6, 7]])
+print(c)
+
+d = np.array([[[1, 2], [3, 4]], [[1, 2], [3, 4]]])
+# print(d)
+print(d.ndim)   # ndim: Number Dimesion/Number of Dimensions
+
+e = np.array([1, 2, 3], ndmin=3)
+print(e)
+
+# np arrays: they usually carry same type of data
+
+f = np.array([1, 2, 3.5])
+print(f)
+
+g = np.array([1, 2, 3.5, "numpy"])
+print(g)
+
+# h = np.array([1, 2, 3], dtype='float')
+h = np.array([1, 2, 3], dtype='complex')
+print(h)
+"""
+"""
+a = [1, 2, 3]
+b = [4, 5, 6]
+print(a + b)
+
+c = np.array([1, 2, 3])
+d = np.array([4, 5, 6])
+print(c + d)
+
+# print(a * b)
+print(c * d)
+
+e = np.array([1, 2, 3], dtype="int16")
+print(e)
+
+print(e.dtype)
+f = np.array([(1, 2)], dtype=[("a", "<i2"), ("b", "<f4")])
+print(f)
+"""
+
+a = np.array([1, 2, 3])
+mat1 = np.mat(a)
+# print(mat1)
+# print(mat1.dtype)
+# print(isinstance(mat1, np.matrix))
+# print(isinstance(a, np.matrix))
+# print(isinstance(a, np.array))
+
+a[0] = 5
+print(a)
+# Next Class: matrix is the subclass of arrays not the reverse is true.
